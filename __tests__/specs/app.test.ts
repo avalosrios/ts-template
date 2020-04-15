e5b300b95f04edee97c0c33bf427c0fa3100234c
Oswaldo Avalos <avalosrios@users.noreply.github.com>
2020-04-15 13:19:24 -0700

    Create nodejs.yml (#4)
    * Create nodejs.yml
    
    * Pass in done callback for tests

--- __tests__/specs/app.test.ts
+++ __tests__/specs/app.test.ts
@@ -1,8 +1,10 @@
 import request from 'supertest';
 import application from '../../src/app';
 describe( 'GET /random-url', () => {
-    it('return 404', () => {
-        request(application.app).get('/random-url')
-            .expect(404);
+    it('return 404', (done) => {
+        return request(application.app)
+            .get('/random-url')
+            .expect(404)
+            .then(() => done());
     });
 });
--- __tests__/specs/v1/api.test.ts
+++ __tests__/specs/v1/api.test.ts
@@ -9,13 +9,14 @@ describe('v1', () => {
     });
 
     describe('when authenticated', () => {
-        it('works', () => {
+        it('works', (done) => {
             return request(application.app).get('/v1')
                 .auth(process.env.API_KEY, process.env.API_SECRET)
                 .expect(200)
                 .then( (res: any) => {
                     expect(res.body.message).toBe('V1 Works!');
-                });
+                })
+                .then(() => done());
         });
     });
 });
